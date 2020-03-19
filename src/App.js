1454b06bb1afb63e46aa79fb511b294ea42fce74
Lin Cong <lcong@sfu.ca>
2020-03-19 06:22:53 -0700

    set up UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Bugs from './components/Bugs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab : 0
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(index){
+    console.log(index)
+    this.setState({
+      activeTab: index
+    });
+  }
+
+  render(){
+    
+    return (
+      <div className="App">
+        <Header handleSelect={this.handleSelect}/>
+        <Bugs activeTab={this.state.activeTab}/>
+      </div>
+    );
+  }
 }
+  
 
 export default App;
