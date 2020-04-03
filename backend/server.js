f6a825cd4b0e4a4717cf1556b4bc6971279721d1
Lin Cong <lcong@sfu.ca>
2020-04-03 16:32:31 -0700

    version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const socketIo = require("socket.io");
+const path = require('path');
 
 require('dotenv').config();
 
@@ -31,6 +32,14 @@ app.use('/api/bugs', bugsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
 
+// serve static assest if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    })
+}
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
