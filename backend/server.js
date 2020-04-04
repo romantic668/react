e69167f677a19feb2902cfc98629e946f68f58c2
Lin Cong <lcong@sfu.ca>
2020-04-03 20:11:40 -0700

    version 1.1.0

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ app.use('/api/auth', authRouter);
 
 // serve static assest if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../build'));
+    app.use('/', express.static(path.join(__dirname, '/../build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
     })
 }
 
